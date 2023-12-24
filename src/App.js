123b450d3c4e5d31bdfadcc7101a597702480e98
Wallewdev <wallewdev@gmail.com>
2023-12-24 13:30:19 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Auth from "./pages/Auth";
+import SignUp from "./components/Signup";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import ForgetPassword from "./components/ForgetPassword";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  
+const App = () => {
   return (
     <>
-      <Auth />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/reset" element={<ForgetPassword />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<h1>This Page is not Found</h1>} />
+      </Routes>
     </>
   );
 }
