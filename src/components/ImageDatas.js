8a0381da9ff95b2e3bc1948b87641b8035f2576f
Wallewdev <wallewdev@gmail.com>
2023-12-24 14:21:27 +0100

    Modification

--- src/components/ImageDatas.js
+++ src/components/ImageDatas.js
@@ -1,15 +1,15 @@
 const imageDatas = [
-    {id: 1, url: 'images/nature-1.jpg', tag: 'Nature'},
+    {id: 10, url: 'images/car-1.jpg', tag: 'Car'},
     {id: 2, url: 'images/nature-2.jpg', tag: 'Nature'},
-    {id: 3, url: 'images/nature-3.jpg', tag: 'Nature'},
-    {id: 4, url: 'images/animal-1.jpg', tag: 'Animal'},
+    {id: 8, url: 'images/lagos-2.jpg', tag: 'City'},
     {id: 5, url: 'images/animal-2.jpg', tag: 'Animal'},
+    {id: 1, url: 'images/nature-1.jpg', tag: 'Nature'},
     {id: 6, url: 'images/animal-3.webp', tag: 'Animal'},
     {id: 7, url: 'images/lagos-1.jpg', tag: 'City'},
-    {id: 8, url: 'images/lagos-2.jpg', tag: 'City'},
-    {id: 9, url: 'images/lagos-3.jpg', tag: 'City'},
-    {id: 10, url: 'images/car-1.jpg', tag: 'Car'},
     {id: 11, url: 'images/car-2.jpg', tag: 'Car'},
+    {id: 4, url: 'images/animal-1.jpg', tag: 'Animal'},
+    {id: 9, url: 'images/lagos-3.jpg', tag: 'City'},
+    {id: 3, url: 'images/nature-3.jpg', tag: 'Nature'},
     {id: 12, url: 'images/car-3.jpg', tag: 'Car'},
 ];
 
